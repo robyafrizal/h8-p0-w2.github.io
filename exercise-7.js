e1ea3247909afb13b68e9ae0672351fdbdfbf2e9
Roby Afrizal Palmendha <robyafrizal86@gmail.com>
2019-08-02 22:34:37 +0700

    Perbaikan Tugas

--- exercise-7.js
+++ exercise-7.js
@@ -1,24 +1,26 @@
 //Menyusun Barisan Bintang
 var rows1 = 5;
-for(var i = 1; i<=rows1; i++){
+for(var i = 0; i < rows1; i++){
     console.log("*");
 }
 
 
 
 var rows2 = 5;
-for(var i = 1; i<=rows2; i++){
-    console.log("");
-    for(var j = 1; j <= rows2; j++){
-        process.stdout.write("*");
+for(var i = 1; i < rows2; i++){
+    var bintang = "";
+    for(var j = 1; j < rows2; j++){
+        bintang += "*";
     }
+    console.log(bintang);
 }
 
 
 var rows3 = 5;
-for(var i = 1; i<=rows3; i++){
-    console.log("");
+for(var i = 1; i < rows3; i++){
+    var bintang = "";
     for(var j = 1; j <= i; j++){
-        process.stdout.write("*");
+        bintang += "*";
     }
+    console.log(bintang);
 }
